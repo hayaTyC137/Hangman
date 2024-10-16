f2b58422151e3e07eda273ed2e617ff3d124f353
hayaTy <kigore17@mail.ru>
2024-10-16 14:41:51 +0300

    Доработанны моменты отображения слова в терминале, теперь висельница отрисовывается нормально и зависит от количества ошибок.

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -1,5 +1,35 @@
+import java.util.*;
 public class GameLogic {
-    public static int counterOfMisses = 0;
+    public static int counterOfMisses;
+    public static char[] hiddenWord;
+
+    public static void guessChar(String givenWord){
+        Scanner sc = new Scanner(System.in);
 
 
+        System.out.println("Введите букву: ");
+        char guessOfPlayer = sc.next().charAt(0);
+        char[] givenWordInArr = givenWord.toCharArray();
+        boolean isCorrect = false;
+
+        if (hiddenWord == null) {
+            char[] hiddenWord = new char[givenWord.length()];
+            // Заполняем подчеркиваниями
+            Arrays.fill(hiddenWord, '_');
+        }
+        for (int i = 0; i < givenWordInArr.length; i++) {
+            if (givenWordInArr[i] == guessOfPlayer) {
+                hiddenWord[i] = guessOfPlayer;
+                isCorrect = true;
+            }
+        }
+        if(!isCorrect){
+            System.out.println("Тут нет такой буквы!");
+            counterOfMisses++;
+        }
+        else {
+            System.out.println("Вы угадали букву");
+        }
+        System.out.println("Загаданное слово: " + new String(hiddenWord));
+    }
 }
--- src/Render.java
+++ src/Render.java
@@ -1,43 +1,45 @@
 import java.util.Scanner;
-
 public class Render {
-    public static void game(){
+    public static void game() {
         boolean gameIsOver = false;
+        Scanner scan = new Scanner(System.in);
+
         while (!gameIsOver) {
-            Scanner scan = new Scanner(System.in);
             System.out.println("Здравствуйте, хотите сыграть в игру?");
             char chosePlayer = scan.next().charAt(0);
+
             if (chosePlayer == 'Y') {
                 System.out.println("Давай");
             } else if (chosePlayer == 'N') {
                 System.out.println("Нет, спасибо");
+                gameIsOver = true;
             }
-            // у меня есть слово, на местах его букв должны отображатся "_"
-            System.out.println("Загаданное слово: " + ChosenWord.getWord().replaceAll(".", "_"));
-            switch (GameLogic.counterOfMisses){
-                case 0:
-                    HangManStages.render_hangman_stages(GameLogic.counterOfMisses);
-                    continue;
-                case 1:
-                    HangManStages.render_hangman_stages(GameLogic.counterOfMisses);
-                    continue;
-                case 2:
-                    HangManStages.render_hangman_stages(GameLogic.counterOfMisses);
-                    continue;
-                case 3:
-                    HangManStages.render_hangman_stages(GameLogic.counterOfMisses);
-                    continue;
-                case 4:
-                    HangManStages.render_hangman_stages(GameLogic.counterOfMisses);
-                    continue;
-                case 5:
-                    HangManStages.render_hangman_stages(GameLogic.counterOfMisses);
-                    continue;
-                case 6:
-                    HangManStages.render_hangman_stages(GameLogic.counterOfMisses);
-                    continue;
-                case 7:
-                    HangManStages.render_hangman_stages(GameLogic.counterOfMisses);
+
+            // Загаданное слово
+            String word = ChosenWord.getWord();
+            System.out.println("Данное слово: " + word.replaceAll(".", "_"));
+            // Устанавливаем начальное состояние игры
+            GameLogic.counterOfMisses = 0;
+            GameLogic.hiddenWord = new char[word.length()]; // Инициализация скрытого слова
+
+            for (int i = 0; i < word.length(); i++) {
+                GameLogic.hiddenWord[i] = '_'; // Заполняем прочерками
+            }
+
+            // Цикл игры
+            while (GameLogic.counterOfMisses < 6 && !new String(GameLogic.hiddenWord).equals(word)) {
+                // Угадываем букву
+                GameLogic.guessChar(word);
+
+                // Рисуем виселицу в зависимости от количества ошибок
+                HangManStages.render_hangman_stages(GameLogic.counterOfMisses);
+            }
+
+            // Результат игры
+            if (GameLogic.counterOfMisses >= 6) {
+                System.out.println("Вы проиграли! Загаданное слово было: " + word);
+            } else {
+                System.out.println("Вы победили!");
             }
         }
     }
